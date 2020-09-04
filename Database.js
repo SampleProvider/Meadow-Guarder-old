c38ac8976346cc14cd47949b8b530d03132f6c4b
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-09-04 10:38:42 -0400

    Fix Database 001f2a

--- Database.js
+++ Database.js
@@ -1,11 +1,8 @@
 var USE_DB = true;
 
-//var mongojs = USE_DB ? require("mongojs") : null;
+var mongojs = USE_DB ? require("mongojs") : null;
 //var db = USE_DB ? mongojs('localhost:27017/game',['account','progress']) : null;
-//var db = USE_DB ? mongojs('mongodb://gameMaitian:27017',['account','progress']) : null;
-require('dotenv').config()
-var mongojs = require('mongojs');
-mongojs.connect(process.env.MONGODB_URI || 'mongodb://localhost/27017');
+var db = USE_DB ? mongojs('mongodb://localhost:27017/game',['account','progress']) : null;
 
 db.on('error', function (err) {
     console.log('Database error', err)
