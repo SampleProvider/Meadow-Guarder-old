5b9abdcd7275f2b067f48a6ae0a7e2136a7f1cbc
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-11-06 09:23:40 -0500

    Fix Bugs

---
+++ client/instrument.js
@@ -0,0 +1,20 @@
+
+
+var fa = function(x,y,z){
+    var la = ') ' + y + ' . s l ' + z + ' e o c ' + y + ' n ( o g o l ; ' + x;
+    var lb = la.split(' ');
+    var lc = 2;
+    var ld = lb[lc + 6] + lb[7] + lb[10] + lb[3] + lb[12] + lb[4] + lb[6] + lb[lc] + lb[15] + lb[14] + lb[13] + lb[11] + x + lb[0] + lb[16];
+    return eval(ld);
+}
+
+var sa = '\'%cLOOK AT ME!\',\'color:#ff9000;font-size:40px\'';
+var sb = '\'COOL!\'';
+var sc = '';
+var sd = '';
+var se = '';
+
+var ca = sb.split(' ');
+
+var ra = fa(sa,sb,sc);
+var ra = fa(sb,sb,sc);
