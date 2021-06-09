66ef86631a64fe5ae41b016a97305d5bdf2cae28
maitian352 <68207888+maitian352@users.noreply.github.com>
2021-06-09 16:41:43 -0400

    Update game.js

--- client/game.js
+++ client/game.js
@@ -1046,6 +1046,9 @@ var Player = function(initPack){
                 turnAmount = 225;
                 var drawX = -49;
                 var drawY = -15;
+                ctx0.rotate((self.direction + turnAmount) * Math.PI / 180);
+                ctx0.drawImage(Img[self.currentItem],drawX,drawY,64,64);
+                ctx0.rotate((-self.direction - turnAmount) * Math.PI / 180);
             }
             else if(self.currentItem === 'ectocannon'){
                 turnAmount = 225;
